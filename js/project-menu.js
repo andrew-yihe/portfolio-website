11e5df382aed72a60ed572644efb99d31bc1814d
andrew-yihe <54768742+andrew-yihe@users.noreply.github.com>
2022-07-25 20:28:38 +1000

    delete the project thumbnail animation on project menu page; updated about me

--- js/project-menu.js
+++ js/project-menu.js
@@ -32,59 +32,59 @@ $(document).ready(function(){
     });
 
     $("#paws-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/paws.png)");
+        $("#block-5").css("background-image", "url(css/paws.png)");
     });
 
     $("#lingo-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/lingo.png)");
+        $("#block-5").css("background-image", "url(css/lingo.png)");
     });
 
     $("#iv-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/iv.png)");
+        $("#block-5").css("background-image", "url(css/iv.png)");
     });
   
     $("#sgm-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/sgm.png)");
+        $("#block-5").css("background-image", "url(css/sgm.png)");
     });
 
     $("#elliot-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/elliot.jpg)");
+        $("#block-5").css("background-image", "url(css/elliot.jpg)");
     });
 
     $("#lighthouse-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/lighthouse.png)");
+        $("#block-5").css("background-image", "url(css/lighthouse.png)");
     });
 
     $("#ball-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/cyber.png)");
+        $("#block-5").css("background-image", "url(css/cyber.png)");
     });
 
     $("#mind-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/mind.png)");
+        $("#block-5").css("background-image", "url(css/mind.png)");
     });
 
     $("#cabaret-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/cabaret.PNG)");
+        $("#block-5").css("background-image", "url(css/cabaret.PNG)");
     });
   
     $("#pumpkin-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/pumpkin.png)");
+        $("#block-5").css("background-image", "url(css/pumpkin.png)");
     });
 
     $("#oasis-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/oasis.jpg)");
+        $("#block-5").css("background-image", "url(css/oasis.jpg)");
     });
 
     $("#cave-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/cave.png)");
+        $("#block-5").css("background-image", "url(css/cave.png)");
     });
 
     $("#neon-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/neon.JPG)");
+        $("#block-5").css("background-image", "url(css/neon.JPG)");
     });
 
     $("#misty-button").hover(function(){
-        $("#block-5").css("background-image", "url(/css/island.jpeg)");
+        $("#block-5").css("background-image", "url(css/island.jpeg)");
     });
 
     new CircleType(document.getElementById('circle-text'));
